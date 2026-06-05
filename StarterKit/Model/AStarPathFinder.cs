f243ad9ecef9824a860615d9c7160c88b890c761
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-06-05 10:57:18 +0200

    working Astar algorithm, had been doing Gscore wrong :(

--- StarterKit/Model/AStarPathFinder.cs
+++ StarterKit/Model/AStarPathFinder.cs
@@ -4,40 +4,82 @@ namespace Model
     {
         PathFinderType _algType = PathFinderType.Astar;
         public PathFinderType algType { get => _algType; set {} }
-        // public PriorityQueue<Node, int>Open{get;set;} = new();
-        public PriorityQueue<int[], int>Open{get;set;} = new();
-        public int[][]? Closed{get;set;}
+
+        public class Node
+        {
+            public int[] Coords {get;set;}
+            public int Heuristic {get;set;}
+            public int Travelled {get;set;}
+            public int Priority => Heuristic + Travelled;
+            public Node? Parent {get;set;}
+        }
+
+        public PriorityQueue<Node, int> Open {get;set;} = new();
+        // public PriorityQueue<int[], int>Open{get;set;} = new();
+        public Dictionary<string, int> Gscore {get;set;} = new();
+        public string Key(int[] pos) => $"{pos[0]}, {pos[1]}";
+
         public int ManhattanDistance(int[] pos, int[] goal)
         {
             return Math.Abs(goal[0] - pos[0]) + Math.Abs(goal[1] - pos[1]);
         }
+
         public void FindPath(Maze maze, int[] pos, Queue<int[]> visitedPositions)
         {
-            int[] goal = maze.End;
-            int steps = 0;
             //start
-            int[] start = {pos[0], pos[1]};
-            Open.Enqueue(start, steps);
+            int[] goal = maze.End;
+            Gscore = new Dictionary<string, int>();
+            Open.Clear();
+            int travelled = 0;
+            Node start = new Node{Coords=pos, Travelled=travelled, Heuristic= ManhattanDistance(pos, goal)};
+            Open.Enqueue(start, start.Priority);
+            Gscore[Key(pos)] = 0;
+
             while(Open.Count > 0)
             {
-                int[] current = Open.Dequeue();
-                visitedPositions.Enqueue(current);
-                if(current[0] == goal[0] && current[1] == goal[1])
+                Node current = Open.Dequeue();
+                string currentKey = Key(current.Coords);
+                
+                if(current.Travelled > Gscore[currentKey]) continue;
+
+                if(current.Coords[0] == goal[0] && current.Coords[1] == goal[1])
                 {
-                    //later
+                    Stack<int[]> path = new Stack<int[]>();
+                    Node temp = current;
+                    while(temp != null)
+                    {
+                        path.Push(temp.Coords);
+                        temp = temp.Parent;
+                    }
+                    while(path.Count > 0)
+                    {
+                        visitedPositions.Enqueue(path.Pop());
+                    }
                     return;
                 }
                 //enqueue valid neigbors
                 foreach (int[] move in maze.moves)
                 {
-                    int[] check = {current[0] + move[0], current[1] + move[1]};
-                    if(maze.IsValidMove(check[0], check[1]))
+                    int[] check = {current.Coords[0] + move[0], current.Coords[1] + move[1]};
+                    if(!maze.IsValidMove(check[0], check[1]))
                     {
-                        steps = steps + 1;
-                        Open.Enqueue(check, steps);
+                        continue;
+                    }
+                    string nextKey = Key(check);
+                    int tentativeG = current.Travelled +1;
+                    if(!Gscore.ContainsKey(nextKey) || tentativeG < Gscore[nextKey])
+                    {
+                        Gscore[nextKey] = tentativeG;
+                        
+                        Node neighbor = new Node(){
+                            Coords= check,
+                            Travelled = current.Travelled + 1,
+                            Heuristic= ManhattanDistance(check, goal),
+                            Parent = current
+                        };
+                        Open.Enqueue(neighbor, neighbor.Priority);
                     }
                 }
-                Closed?.Append(current);
             }
         }
     }
